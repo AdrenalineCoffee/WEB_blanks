fe0a7fb88c9c4144e8380449074a2f1bd8fef9f8
Евгений <AdrenalineCoffee@users.noreply.github.com>
2019-08-08 13:39:58 +0300

    replaceAlphabet
     Программа заменяет все английские символы на порядковые номера их в алфавите

---
+++ replaceAlphabet.js
@@ -0,0 +1,18 @@
+function alphabetPosition(text) {
+    var alph = [ '', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+    'w', 'x', 'y', 'z'];
+    var numarr = '';
+    
+    text = text.toLowerCase();
+
+    function swit(element){
+        if(alph.indexOf(element) != '-1'){ 
+        numarr = numarr + ' ' + alph.indexOf(element);
+        numarr = numarr.trim();
+        } 
+    }
+    text.split('').forEach(swit);
+    return numarr;
+  }
+
+  console.log(alphabetPosition('The quick brown fox jumps over the lazy dog.'));
