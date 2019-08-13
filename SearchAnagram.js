943eff34c4f91db660177c4b2f85a304bb38f7eb
Евгений <AdrenalineCoffee@users.noreply.github.com>
2019-08-13 14:57:12 +0300

    SearhAnagram
    Программа находит анаграммы для выделенного слова

---
+++ SearchAnagram.js
@@ -0,0 +1,13 @@
+function anagrams(word, words) {
+  var result = [];
+  for(var element in words){
+    if(words[element].length == word.length){
+      if(word.split('').sort().join() == words[element].split('').sort().join()){
+        result.push(words[element]);
+      }
+    }
+  }
+  return result;
+}
+
+console.log(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']));
