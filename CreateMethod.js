8149b7394b27510b53534de046685edf35202bf9
Евгений <AdrenalineCoffee@users.noreply.github.com>
2019-08-14 14:10:41 +0300

    Update CreateMethod.js
    Добавил обработчики ошибок и метод для получения значения

--- CreateMethod.js
+++ CreateMethod.js
@@ -1,28 +1,58 @@
-  function date(time) {
-  var sdf = new Date(Date.parse(time)); 
+  function date(date) {
+    var sdf = new Date(Date.parse(date)); 
 
-      return {
-      add: function(param, time) {
-
-        if (time == 'minutes') sdf.setMinutes(sdf.getMinutes() + param);  
-        if (time == 'hours') sdf.setHours(sdf.getHours() + param);
-        if (time == 'days') sdf.setDate(sdf.getDay() + param);
-        if (time == 'months') sdf.setMonth(sdf.getMonth() + param);
-        if (time == 'years') sdf.setFullYear(sdf.getFullYear() + param);
-        return this;
-      },
-
-      subtract: function(param, time){
-
-        if (time == 'minutes') sdf.setMinutes(sdf.getMinutes() - param);  
-        if (time == 'hours') sdf.setHours(sdf.getHours() - param);
-        if (time == 'days') sdf.setDate(sdf.getDay() - param);
-        if (time == 'months') sdf.setMonth(sdf.getMonth() - param);
-        if (time == 'years') sdf.setFullYear(sdf.getFullYear() - param);
-        return this;
-      },
-    
+    return {
+    add: function(param, time) {
+        if(param >=0 ){
+      switch(time){
+        case ('minutes'): sdf.setMinutes(sdf.getMinutes() + param);  break; 
+        case ('hours'): sdf.setHours(sdf.getHours() + param); break;
+        case ('days'): sdf.setHours(sdf.getHours() + param*24); break;
+        case ('months'): sdf.setMonth(sdf.getMonth() + param); break;
+        case ('years'): sdf.setFullYear(sdf.getFullYear() + param); break;
+        default: throw new TypeError('Неизвестная единица времени');
+      }
+      return this;
     }
+    else{ throw new TypeError('Время не может быть отрицательным!');}
+    },
+
+    subtract: function(param, time){
+        if(param >=0 ){
+      switch(time){
+        case ('minutes'): sdf.setMinutes(sdf.getMinutes() - param);   break;
+        case ('hours'): sdf.setHours(sdf.getHours() - param); break;
+        case ('days'): sdf.setHours(sdf.getHours() - param*24); break;
+        case ('months'): sdf.setMonth(sdf.getMonth() - param); break;
+        case ('years'): sdf.setFullYear(sdf.getFullYear() - param); break;
+        default: throw new TypeError('Неизвестная единица времени');
+      }
+      return this;
+    }
+    else{ throw new TypeError('Время не может быть отрицательным!');}
+    },
+    get value() {
+      //2017-04-20 14:00
+      var d = sdf,
+          month = '' + (d.getMonth() + 1),
+          day = '' + d.getDate(),
+          year = '' + d.getFullYear(),
+          hour = '' + d.getHours(),
+          minute = '' + d.getMinutes();
+
+
+      if (month.length < 2) month = '0' + month;
+      if (day.length < 2) day = '0' + day;
+      if (hour.length < 2) hour = '0' + hour;
+      if (minute.length < 2) minute = '0' + minute;
+
+      var fullyear = [year, month, day].join('-');
+      var fulltime = [hour, minute].join(':');
+      return fullyear + " " + fulltime;
+
+  },
+  
+  }
   }
 
 
@@ -35,5 +65,5 @@ var time = date('2017-05-16 13:45')
 
 .add(15, 'minutes');
 //console.log(date(['2017-05-16 13:45']));
-console.info(time.sdf);
+console.info(time.value);
 
